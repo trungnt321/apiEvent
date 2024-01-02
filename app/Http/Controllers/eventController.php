75f191472a4d86b3294c5e321b81d54f184cd39b
haudv3202 <haudang02.it@gmail.com>
2024-01-02 13:02:45 +0700

    fix put change post update event

--- app/Http/Controllers/eventController.php
+++ app/Http/Controllers/eventController.php
@@ -1189,7 +1189,7 @@ class eventController extends Controller
     public function update(Request $request, $id)
     {
         //Check validate
-//        dd($request->all());
+        dd($request->all());
         $validate = Validator::make($request->all(), [
             'name' => 'required',
             'location' => ['required'],
